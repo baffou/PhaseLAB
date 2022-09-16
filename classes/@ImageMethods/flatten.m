2146f59ce0319284b01a09bcfcf3a3d237f075f0
Guillaume Baffou <guillaume.baffou@fresnel.fr>
2022-09-16 13:19:47 +0200

    small update of the flatten method

--- classes/@ImageMethods/flatten.m
+++ classes/@ImageMethods/flatten.m
@@ -1,5 +1,8 @@
 function obj2 = flatten(obj,method,nmax)
-methodList={'Zernike','Waves'};
+% methods:
+%   'Zernike'
+%   or
+%   'Waves', 'Wave', 'Sine', 'sine'
 
 if nargout
     obj2=copy(obj);
@@ -9,11 +12,7 @@ end
 
 if nargin==1 % if obj = flatten(3), then consider flatten('Zernike',3)
     nmax=method;
-    method='Zernike';
-end
-
-if ~ismember(method,methodList)
-    error('Wrong method')
+    method='Waves';
 end
 
 
@@ -43,7 +42,7 @@ if strcmp(method,'Zernike')
         end
         obj2(io).OPD=temp;
     end
-elseif strcmp(method,'Waves')
+elseif strcmpi(method,'Waves') || strcmpi(method,'Wave') || strcmpi(method,'Sines') || strcmpi(method,'Sine')
     No = numel(obj);
     for io = 1:No
         temp=obj(io).OPD;
